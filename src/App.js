f06a820f3035ae8fa600861be049f31dd5036b3b
krsnananda <krsnananda@outlook.com>
2020-01-05 03:53:13 -0300

    - Criação de menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import Popup from './Popup';
 
-function App() {
+export default class App extends Component {
+  constructor(props){  
+    super(props);  
+    this.state = { showPopup: false };  
+    }  
+    
+      togglePopup() {  
+    this.setState({  
+         showPopup: !this.state.showPopup  
+    });  
+     }  
+    
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+        <ul class="menu">
+          <li><a href="#!">About me</a></li>
+          <li><a href="#!">Skills</a></li>
+          <li><a href="#!">Experience</a></li>
+          <li><a href="#!">Contact</a></li>
+        </ul>
+      </nav>
       </header>
+      <div>  
+        <h1> Simple Popup Example In React Application </h1>  
+        <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>  
+        <Popup  
+          text='Click "Close Button" to hide popup'  
+          closePopup={this.togglePopup.bind(this)}  
+          />  
+      </div>
     </div>
   );
 }
+}
 
-export default App;
